c5e6d70a722840a86010c4f3a869b3811492310c
rifat32 <rifatbilalphilips@gmail.com>
2022-06-18 21:18:15 +0600

    update

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const APPNAME = "Appointment";
 //  export const BACKEND = "http://127.0.0.1:8000";
 // export const BACKENDAPI = "http://127.0.0.1:8000/api";
- export const BACKEND = "http://bdpetcare.com/pos/pos/create";
+export const BACKEND = "http://bdpetcare.com/doctor";
 export const BACKENDAPI = `${BACKEND}/api`;
